9fdf553eabf8d96f90d3e158753fab595f7755e7
lyem <rfael403@gmail.com>
2022-03-20 22:05:39 -0300

    create update people request

---
+++ app/Http/Requests/People/UpdatePeopleRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests\People;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdatePeopleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        $user = auth()->user();
+        return $user->update_people || $user->owner;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'max:32|min:2|string|unique:App\Models\People,name',
+            'birth' => 'date',
+            'gender' => 'string',
+            'photo' => 'image',
+            'about' => 'string|max:300|min:20',
+            'twitter' => 'string',
+            'facebook' => 'string',
+            'instagram' => 'string',
+            'anilist' => 'string',
+            'myanimelist' => 'string',
+            'youtube' => 'string',
+            'website' => 'string'
+        ];
+    }
+}
