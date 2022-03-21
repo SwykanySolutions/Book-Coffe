517f0e7c6f61769548e82b6e7350cd794ddbe7ab
lyem <rfael403@gmail.com>
2022-03-20 22:06:13 -0300

    create category request

---
+++ app/Http/Requests/Category/CreateCategoryRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Category;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateCategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        $user = auth()->user();
+        return $user->create_category || $user->owner;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:32|min:2|string|unique:App\Models\People,name',
+        ];
+    }
+}
