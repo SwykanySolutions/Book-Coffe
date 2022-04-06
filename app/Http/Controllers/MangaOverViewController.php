4f2dbd02517a9e84ca5e7a50cf4c999c5a338685
lyem <rfael403@gmail.com>
2022-04-06 00:53:49 -0300

    create manga

--- app/Http/Controllers/MangaOverViewController.php
+++ app/Http/Controllers/MangaOverViewController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\MangaOverView;
+use App\Http\Requests\Manga\CreateMangaOverViewRequest;
 
 class MangaOverViewController extends Controller
 {
@@ -42,9 +43,19 @@ class MangaOverViewController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CreateMangaOverViewRequest $request)
     {
-        //
+        $infos = $request->all();
+        if($photo = $request->file('photo')) $infos['photo'] = $photo->store('manga_photo', 'public');
+        if($photo = $request->file('background_photo')) $infos['background_photo'] = $photo->store('manga_background_photo', 'public');
+        unset($infos["status"]);
+        unset($infos["format"]);
+        $manga = $this->manga->create($infos);
+        $manga->categories()->attach(array_map('intval', $request->categories));
+        $manga->people()->attach(array_map('intval', $request->staffs));
+        $manga->statuses()->save(\App\Models\Status::find((int)$request->status));
+        $manga->formats()->save(\App\Models\Format::find((int)$request->format));
+        return $this->manga->find($manga->id);
     }
 
     /**
