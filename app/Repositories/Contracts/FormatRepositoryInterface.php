529f33b855f4ea7cf3862a43a0ae1e739664582a
lyem <rfael403@gmail.com>
2022-06-21 16:11:22 -0300

    create FormatRepositoryInterface

---
+++ app/Repositories/Contracts/FormatRepositoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+use App\Models\Format;
+
+interface FormatRepositoryInterface
+{
+    public function getFormatbyId(int $id);
+    public function createFormat(array $request);
+    public function updateFormat(array $request, int $id);
+    public function deleteFormat(Format $format);
+}
