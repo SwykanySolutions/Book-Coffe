444cb6c688808221d42e00a65102136143dec98a
lyem <rfael403@gmail.com>
2022-06-21 16:13:26 -0300

    create categoryRepository

---
+++ app/Repositories/CategoryRepository.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Repositories\Contracts\CategoryRepositoryInterface;
+use App\Models\Category;
+
+class CategoryRepository implements CategoryRepositoryInterface
+{
+
+    protected $category;
+
+    public function __construct(Category $category)
+    {
+        $this->category = $category;
+    }
+
+    public function getCategorybyId(int $id)
+    {
+        return $this->category->find($id);
+    }
+
+    public function creteCategory(array $request)
+    {
+        return $this->category->create($request);
+    }
+
+    public function updateCategory(array $request, int $id)
+    {
+        $category = $this->category->find($id);
+        $category->update($request);
+        return $this->category->find($id);
+    }
+
+    public function deleteCategory(Category $category)
+    {
+        $category->delete();
+    }
+
+}
