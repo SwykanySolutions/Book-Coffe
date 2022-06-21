ad9545ad1b6e76278372d539257c50b0a12fb024
lyem <rfael403@gmail.com>
2022-06-21 16:08:58 -0300

    create FormatService

---
+++ app/Services/FormatService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\Contracts\FormatRepositoryInterface;
+
+class FormatService
+{
+
+    protected $formatRepository;
+
+    public function __construct(FormatRepositoryInterface $formatRepository)
+    {
+        $this->formatRepository = $formatRepository;
+    }
+
+    public function createFormat(array $request)
+    {
+        return $this->formatRepository->createFormat($request);
+    }
+
+    public function updateFormat(array $request, int $id)
+    {
+        return $this->formatRepository->updateFormat($request, $id);
+    }
+
+    public function deleteFormat(int $id)
+    {
+        $user = auth()->user();
+        if($user->delete_format || $user->owner){
+            $format = $this->formatRepository->getFormatbyId($id);
+            if(!$format) return abort(404);
+            $this->formatRepository->deleteFormat($format);
+        } else {
+            return abort(403,'Unauthorized action.');
+        }
+    }
+
+}
