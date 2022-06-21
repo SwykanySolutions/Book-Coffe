dd9f6cee7464d77eb3f78bbfdb4bad4cbf72104d
lyem <rfael403@gmail.com>
2022-06-21 16:09:48 -0300

    create userRepositoryInterface

---
+++ app/Repositories/Contracts/UserRepositoryInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+use App\Models\User;
+
+interface UserRepositoryInterface
+{
+    public function listAllUsers();
+    public function createUser(array $request);
+    public function updateUser(User $user, array $request);
+    public function getUserbyName(string $tag);
+    public function getUserbyId(int $id);
+    public function getUserbyEmail(string $email);
+    public function deleteUser(int $id);
+}
