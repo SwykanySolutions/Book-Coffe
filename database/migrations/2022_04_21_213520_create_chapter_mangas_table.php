a67ef47e7de8d046e60cd5ccc178047dd90df0f1
lyem <rfael403@gmail.com>
2022-06-21 15:55:02 -0300

    create manga chapters migration

---
+++ database/migrations/2022_04_21_213520_create_chapter_mangas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chapter_mangas', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable();
+            $table->string('chapter');
+            $table->foreignId('user_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->foreignId('manga_over_view_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chapter_mangas');
+    }
+};
