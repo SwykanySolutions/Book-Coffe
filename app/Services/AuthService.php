18ab35a4ca2194bb472794e0e37c112ad167cb30
lyem <rfael403@gmail.com>
2022-07-25 14:03:42 -0300

    add token login and web login

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -3,8 +3,9 @@
 namespace App\Services;
 
 use App\Repositories\Contracts\UserRepositoryInterface;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthService
 {
@@ -23,20 +24,32 @@ class AuthService
 
     public function login(Request $request)
     {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return auth()->user();
+        }
+
+        abort(401);
+    }
+
+    public function loginToken(Request $request)
+    {
         $user = $this->userRepository->getUserbyEmail($request->email);
-        if(!$user)
-        {
+        if (!$user) {
             return abort(404);
         }
-        if(Hash::check($request->password, $user->password))
-        {
+        if (Hash::check($request->password, $user->password)) {
             $token = $user->createToken($request->header('User-Agent'));
             $user['token'] = $token->plainTextToken;
             return $user;
-        }
-        else
-        {
-            return abort(403,'Unauthorized action.');
+        } else {
+            return abort(403, 'Unauthorized action.');
         }
     }
 
