9ece48e51bd03e7de49ff8e5b83018a3b38e6080
lyem <rfael403@gmail.com>
2022-08-02 00:49:13 -0300

    create manga score request

---
+++ app/Http/Requests/Manga/Score/CreateMangaScoreRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Manga\Score;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateMangaScoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        $user = auth()->user();
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "manga_id" => "required|numeric",
+            "score" => "required|integer|between:1,10",
+        ];
+    }
+}
